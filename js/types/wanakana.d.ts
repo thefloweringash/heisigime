9368ad63b3156e768305a10c4414979a5b6ad6c5
Andrew Childs <lorne@cons.org.nz>
2023-10-06 16:26:40 +0900

    types: add Wanakana.toRomaji

--- js/types/wanakana.d.ts
+++ js/types/wanakana.d.ts
@@ -1,5 +1,6 @@
 declare module "wanakana" {
   export const toHiragana: (input: string) => string;
   export const toKatakana: (input: string) => string;
+  export const toRomaji: (input: string) => string;
   export const isKana: (input: string) => boolean;
 }
